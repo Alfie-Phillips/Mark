672f9c6e0a6457123f8a413438ceac9796a17b0e
Alfie Phillips <thealfiephillips@gmail.com>
2021-05-01 09:22:49 +0000

    keep alive

--- keep_alive.py
+++ keep_alive.py
@@ -5,7 +5,7 @@ app = Flask('')
 
 @app.route('/')
 def home():
-    return redirect("https://discord.gg/HHeYChX8")
+    return "Bot is running!"
 
 def run():
     app.run(host="0.0.0.0", port=8080)
